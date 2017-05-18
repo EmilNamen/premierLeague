e29faa39197591de596e108478b5ef12c134d0b7
Emil Namen <ec.namen1091@uniandes.edu.co>
2017-05-18 16:41:48 -0500

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from getOutputs import getOutputs
 
 homeNamePremierLeagueWebPage = ['Tottenham Hotspur','Arsenal','Chelsea','Manchester United','Liverpool','Manchester City','Everton','Southhampton']
 homeNameCSV = ['Tottenham','Arsenal','Chelsea','Man United','Liverpool','Man City','Everton','Southhampton']
+homeNameNewCSV = ['Tottenham Hotspur','Arsenal FC','Chelsea FC','Manchester United','Liverpool FC','Manchester City','Everton FC','Southhampton FC']
 visitorNamePremierLeagueWebPage = ['Arsenal','Tottenham Hotspur','Manchester United','Chelsea','Manchester City','Liverpool','Southhampton','Everton']
 visitorNameCSV = ['Arsenal','Tottenham','Man United','Chelsea','Man City','Liverpool','Southhampton','Everton']
+visitorNameNewCSV = ['Arsenal FC','Tottenham Hotspur','Manchester United','Chelsea FC','Manchester City','Liverpool FC','Southhampton FC','Everton FC']
 matrix = [[0 for i in range(12)] for j in range(12)]
 seasons = ['2000s/2002-03','2000s/2003-04','2000s/2004-05','2000s/2005-06','2000s/2006-07','2000s/2007-08','2000s/2008-09','2000s/2009-10','2010s/2010-11','2010s/2011-12','2010s/2012-13', '2010s/2013-14']
 outputs = [0,0,0]
@@ -22,4 +24,4 @@ for i,team in enumerate(homeNamePremierLeagueWebPage):
     #print getTopSaves(team, visitorNamePremierLeagueWebPage[i], matrix, i)   #matrix[4] & matrix[5]
     #for season in seasons:
         #print readCSV(homeNameCSV[i],visitorNameCSV[i],season,matrix,i)
-    print readnewCSV2015(homeNameCSV[i],visitorNameCSV[i],matrix,i)
+    print readnewCSV2015(homeNameNewCSV[i],visitorNameNewCSV[i],matrix,i)
